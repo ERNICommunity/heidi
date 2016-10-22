38e600560dbe7df64b8fdf752fdf007f09aa85b1
mis <mis@NB-ECH-MIS>
2016-10-22 23:30:38 +0200

    Application uses static files, default files and MVC

--- Clara/src/Clara/Startup.cs
+++ Clara/src/Clara/Startup.cs
@@ -12,6 +12,7 @@ namespace Clara
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -23,11 +24,9 @@ namespace Clara
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseMvc();
         }
     }
 }
